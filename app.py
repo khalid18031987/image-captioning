debbd5131d5b2fd88fb8a5c96e9079b087f3baf0
khalid18031987 <khalid1803@gmail.com>
2025-05-26 23:25:36 +0100

    app.py

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ import streamlit as st
 from imgcaptioning.pipeline import pipeline
 
 st.set_page_config(
-    page_title="Image Caption Generator",
+    page_title="Application Image-to-Text",
     page_icon="📸",
     menu_items={
-        'Report a bug': "https://github.com/pnkvalavala/image-captioning/issues",
         'About': "Just upload image and generate captions for free"
     }
 )
 
+# 👉 Affichage de la bannière en haut
+st.image("bann.gif", use_column_width=True)
+
+# 👉 Titre principal
 st.markdown(
     "<h1 style='text-align: center;'>Image Caption Generator</h1>",
     unsafe_allow_html=True
